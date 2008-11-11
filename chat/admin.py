3b2b61674a5cb41d1e3151a5700f63a1a241f65a
Will McCutchen <mccutchen@gmail.com>
2008-11-11 02:04:57 -0600

    Added initial cut at the chat app

---
+++ chat/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from watercooloer.chat.models import Chat, Post
+
+admin.site.register(Author)
+admin.site.register(Post)
---
+++ chat/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Chat(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(prepopulate_from='name', unique=True)
+    public = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Post(models.Model):
+    chat = models.ForeignKey(Chat)
+    user = models.ForeignKey(User)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
