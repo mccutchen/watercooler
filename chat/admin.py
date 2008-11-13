9cef7cc9827ee9ab49cd646c6afd7811eb262d15
Will McCutchen <mccutchen@gmail.com>
2008-11-12 19:36:27 -0600

    Prepopulate the slug field for chats

--- chat/admin.py
+++ chat/admin.py
@@ -4,6 +4,7 @@ from watercooler.chat.models import Chat, Post
 class ChatAdmin(admin.ModelAdmin):
     list_display = ('name', 'created', 'created_by', 'is_public')
     list_filter = ('is_public',)
+    prepopulated_fields = {'slug': ('name',)}
 
 class PostAdmin(admin.ModelAdmin):
     exclude = ('content_rendered',)
