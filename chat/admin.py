483eb456105c948c7dabf049e72a2d2283419ae3
Will McCutchen <mccutchen@gmail.com>
2008-11-12 19:31:29 -0600

    Created custom Admin objects for Chat and Post

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from watercooler.chat.models import Chat, Post
 
-admin.site.register(Chat)
-admin.site.register(Post)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'created_by', 'is_public')
+    list_filter = ('is_public',)
+
+class PostAdmin(admin.ModelAdmin):
+    exclude = ('content_rendered',)
+    list_display = ('user', 'timestamp', 'parent')
+
+admin.site.register(Chat, ChatAdmin)
+admin.site.register(Post, PostAdmin)
