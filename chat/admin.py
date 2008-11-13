4a6eb5d65edb779d6bc1aeccc3a9bbf51c67e6a0
Will McCutchen <mccutchen@gmail.com>
2008-11-12 20:11:01 -0600

    Add a PostInline object so posts can be edited on the chat page

--- chat/admin.py
+++ chat/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from watercooler.chat.models import Chat, Post
 
+class PostAdmin(admin.ModelAdmin):
+    exclude = ('content_rendered',)
+    list_display = ('user', 'timestamp', 'parent')
+
+class PostInline(admin.StackedInline):
+    model = Post
+    exclude = ['content_rendered']
+
 class ChatAdmin(admin.ModelAdmin):
     list_display = ('name', 'created', 'created_by', 'is_public')
     list_filter = ('is_public',)
     prepopulated_fields = {'slug': ('name',)}
-
-class PostAdmin(admin.ModelAdmin):
-    exclude = ('content_rendered',)
-    list_display = ('user', 'timestamp', 'parent')
+    inlines = [PostInline]
 
 admin.site.register(Chat, ChatAdmin)
 admin.site.register(Post, PostAdmin)
