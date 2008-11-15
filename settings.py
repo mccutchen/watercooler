2e5a3c4b30b94509f17b9c5bcddbe921070e2d25
Will McCutchen <mccutchen@gmail.com>
2008-11-15 17:22:57 -0600

    Added base templates and index page

--- settings.py
+++ settings.py
@@ -41,12 +41,12 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'http://localhost/~mccutchen/media/watercooler/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = 'http://localhost/~mccutchen/media/admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '2*pb$jp-x2x-!6$06jmq=f_!rhg-tc(+3bersw$j@e+h5*i787'
@@ -55,6 +55,10 @@ SECRET_KEY = '2*pb$jp-x2x-!6$06jmq=f_!rhg-tc(+3bersw$j@e+h5*i787'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.media',
 )
 
 MIDDLEWARE_CLASSES = (
--- urls.py
+++ urls.py
@@ -1,16 +1,10 @@
 from django.conf.urls.defaults import *
-
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^watercooler/', include('watercooler.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    (r'^$', direct_to_template, {'template': 'index.html'}),
+    (r'^chat/', include('chat.urls')),
     (r'^admin/(.*)', admin.site.root),
 )
