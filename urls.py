7c68f782f47864ead9ad7238990e631d2e2673d4
Will McCutchen <mccutchen@gmail.com>
2008-11-15 17:40:36 -0600

    List public chats on the index page

--- urls.py
+++ urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 from django.contrib import admin
+from watercooler.chat.models import Chat
+
 admin.autodiscover()
 
+index_options = {
+    'template': 'index.html',
+    'extra_context': {
+        'public_chats': Chat.objects.filter(is_public=True),
+    },
+}
+
 urlpatterns = patterns('',
-    url(r'^$', direct_to_template, {'template': 'index.html'}, name='index'),
+    url(r'^$', direct_to_template, index_options, name='index'),
     url(r'^contact/$', direct_to_template, {'template': 'contact.html'}, name='contact'),
     (r'^chat/', include('chat.urls')),
     (r'^admin/(.*)', admin.site.root),
