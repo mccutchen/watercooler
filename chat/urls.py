8fca1c281179a845883e7d1f0c1ae2fc1aa4c4ad
Will McCutchen <mccutchen@gmail.com>
2008-11-15 18:42:05 -0600

    Use generic object_detail view to display chats

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls.defaults import *
-import views
+from django.views.generic.list_detail import object_list, object_detail
+from watercooler.chat.models import Chat
+
+info_dict = {
+    'queryset': Chat.objects.all(),
+    'template_object_name': 'chat',
+}
 
 urlpatterns = patterns('',
-    (r'^chat/$', views.index),
-    (r'^chat/([\w\-_]+)/$', views.chat),
-    (r'^chat/([\w\-_]+)/post/$', views.post),
+    url(r'^(?P<slug>[-\w]+)/$', object_detail, info_dict, name='chat_detail'),
+    #(r'^chat/$', views.index),
+    #(r'^chat/([\w\-_]+)/$', views.chat),
+    #(r'^chat/([\w\-_]+)/post/$', views.post),
 )
