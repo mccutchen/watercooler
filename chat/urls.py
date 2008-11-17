fd918c06540086b79ff4e1ba391a3d496e048ef0
Will McCutchen <mccutchen@gmail.com>
2008-11-16 21:02:58 -0600

    Allow users to create their own chats

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_list, object_detail
 
-from chat.views import chat, post
+from chat.views import chat, post, create
 
 urlpatterns = patterns('',
+    url(r'^create/$', create, name='chat_create'),
     url(r'^(?P<slug>[-\w]+)/$', chat, name='chat'),
     url(r'^(?P<slug>[-\w]+)/post/', post, name='chat_post'),
     #(r'^chat/$', views.index),
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,8 @@
 from django.views.generic.list_detail import object_list, object_detail
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseServerError
+from django.template.defaultfilters import slugify
 
 from chat.models import Chat, Post
 
@@ -21,4 +22,18 @@ def post(request, slug):
         chat = Chat.objects.get(slug=slug)
         post = Post(user=request.user, parent=chat, content=content)
         post.save()
-    return HttpResponseRedirect(reverse('chat', args=[slug]))
+    return HttpResponseRedirect(chat_url(slug))
+
+@login_required
+def create(request):
+    name = request.POST['name']
+    slug = slugify(name)
+    print 'Creating chat named "%s" with slug "%s"' % (name, slug)
+    chat = Chat(name=name, slug=slug, is_public=True, created_by=request.user)
+    chat.save()
+    return HttpResponseRedirect(chat_url(slug))
+
+def chat_url(slug):
+    """Utility function that uses the reverse() function to generate the
+    correct URL for the Chat object with the given slug."""
+    return reverse('chat', args=[slug])
