2f30686ae02266e7dd5f0e5d89f392d170cbc48a
Will McCutchen <mccutchen@gmail.com>
2008-11-16 20:36:13 -0600

    Let users add their own posts to chats

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_list, object_detail
 
-from chat.views import chat_detail
+from chat.views import chat_detail, post
 
 urlpatterns = patterns('',
     url(r'^(?P<slug>[-\w]+)/$', chat_detail, name='chat_detail'),
+    url(r'^(?P<slug>[-\w]+)/post/', post, name='chat_post'),
     #(r'^chat/$', views.index),
     #(r'^chat/([\w\-_]+)/$', views.chat),
     #(r'^chat/([\w\-_]+)/post/$', views.post),
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,9 @@
 from django.views.generic.list_detail import object_list, object_detail
 from django.contrib.auth.decorators import login_required
-from chat.models import Chat
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+from chat.models import Chat, Post
 
 @login_required
 def chat_detail(request, slug):
@@ -10,3 +13,12 @@ def chat_detail(request, slug):
         slug=slug,
         template_object_name='chat'
     )
+
+@login_required
+def post(request, slug):
+    if request.POST:
+        content = request.POST.get('content', '')
+        chat = Chat.objects.get(slug=slug)
+        post = Post(user=request.user, parent=chat, content=content)
+        post.save()
+    return HttpResponseRedirect(reverse('chat_detail', args=[slug]))
