99b174b72ba6d87f157e429363c5ead27eb9c4f8
Will McCutchen <mccutchen@gmail.com>
2008-11-17 18:06:26 -0600

    Added index of public chats

--- chat/urls.py
+++ chat/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls.defaults import *
-from django.views.generic.list_detail import object_list, object_detail
+from django.views.generic.list_detail import object_list
 
-from chat.views import chat, post, create
+from models import Chat
+from views import chat, post, create
+
+chat_list_options = {
+    'queryset': Chat.objects.public(),
+    'template_object_name': 'chat',
+}
 
 urlpatterns = patterns('',
+    url(r'^$', object_list, chat_list_options, name='chat_index'),
     url(r'^create/$', create, name='chat_create'),
     url(r'^(?P<slug>[-\w]+)/$', chat, name='chat'),
     url(r'^(?P<slug>[-\w]+)/post/', post, name='chat_post'),
-    #(r'^chat/$', views.index),
-    #(r'^chat/([\w\-_]+)/$', views.chat),
-    #(r'^chat/([\w\-_]+)/post/$', views.post),
 )
