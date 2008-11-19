d3bd3f3cca6b06df4437262dbd2fdf1ae47cebb0
Will McCutchen <mccutchen@gmail.com>
2008-11-19 13:26:48 -0600

    Added utils.js

---
+++ media/js/utils.js
@@ -0,0 +1,30 @@
+Array.prototype.each = function(fn) {
+    for (var i = 0; i < this.length; i++) {
+        fn(this[i]);
+    }
+};
+Array.prototype.map = function(fn) {
+    var results = [];
+    for (var i = 0; i < this.length; i++) {
+        results.push(fn(this[i]));
+    }
+    return results;
+};
+Array.prototype.filter = function(test) {
+    test = test || function(el) { return el ? true : false; };
+    var results = [];
+    for (var i = 0; i < this.length; i++)
+        if (test(this[i]))
+            results.push(this[i]);
+    return results;
+};
+Array.prototype.any = function(test) {
+    return this.filter(test).length > 0;
+};
+
+
+if (!console) {
+    var console = {
+        log: function() {}
+    };
+}
