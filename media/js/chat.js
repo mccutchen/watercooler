db3b9b0eeeef00d583de7f369db224914f3d9ab4
Will McCutchen <mccutchen@gmail.com>
2008-11-18 18:31:16 -0600

    Don't submit the post form if the content is blank

--- media/js/chat.js
+++ media/js/chat.js
@@ -1,8 +1,17 @@
 $(function() {
-    $('#post-content').keypress(function(e) {
-        if (e.which == 13 && !e.shiftKey) {
-            document.forms['post-form'].submit();
-            return false;
+    // Only submit the post if it is not blank.
+    $('#post-form').submit(function(event) {
+        var blank = /^\s*$/;
+        event.preventDefault();
+        return !blank.test(this['content'].value);
+    });
+    
+    // Submit the post automatically if the user presses
+    // the enter key.
+    $('#post-content').keypress(function(event) {
+        if (event.which == 13 && !event.shiftKey) {
+            event.preventDefault();
+            return $('#post-form').submit();
         }
     });
 });
