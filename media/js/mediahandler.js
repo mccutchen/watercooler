8c9d127ff3a8bc25849dd3cd91e3595cea8c3658
Will McCutchen <mccutchen@gmail.com>
2008-11-19 14:14:08 -0600

    Support replacing media objects surrounded by other text

--- media/js/mediahandler.js
+++ media/js/mediahandler.js
@@ -9,7 +9,7 @@ var MediaHandler = (function() {
         });
     }
     
-    var url_re = /((http:\/\/|www\.){1,2}(www\.)?[A-z0-9\-.]+\.([A-z]{2,4}\.?)+[^\s]*)/;
+    var url_re = /((http:\/\/|www\.){1,2}(www\.)?[A-z0-9\-.]+\.([A-z]{2,4}\.?)+[^\s]*)/i;
     var img_re = /\.(jpg|jpeg|gif|png)$/i;
     var youtube_re = /^http:\/\/www\.youtube\.com\/watch\?v=([A-z0-9]{11})/i;
     var audio_re = /\.(mp3|aac)$/i;
@@ -18,7 +18,7 @@ var MediaHandler = (function() {
         return '<a href="' + url + '"><img src="' + url + '" alt="" /></a>';
     });
     makehandler('youtube', youtube_re, function(url, match) {
-        return '<embed src="http://www.youtube.com/v/' + match + '" type="application/x-shockwave-flash" width="300" height="200" />';
+        return '<embed src="http://www.youtube.com/v/' + match[1] + '" type="application/x-shockwave-flash" width="300" height="200" />';
     });
     makehandler('audio', audio_re, function(url, match) {
         return '<embed src="http://static.overloaded.org/watercooler/mediaplayer/player.swf" type="application/x-shockwave-flash" flashvars="file=' + url + '&amp;backcolor=222222&amp;frontcolor=FFFFFFF&amp;lightcolor=999999" height="20" width="50%" />';
@@ -45,15 +45,23 @@ var MediaHandler = (function() {
         if (!url)
             return content;
         
+        // Any transformation done by a media handler will be stored here.
+        var result;
+        
         // Try to find a handler that matches the URL we're working on
         for (var i = 0; i < handlers.length; i++) {
             var handler = handlers[i];
-            if (match = url.match(handler.re))
-                return handler.fn(url, match[1]);
+            if (match = url.match(handler.re)) {
+                result = handler.fn(url, match);
+                break;
+            }
         }
-
+        
         // If no handler was found, use the default handler
-        return handlers.default(url);
+        result = result || handlers.default(url);
+        
+        // Replace the URL
+        return content.replace(url_re, result);
     }
     
     return { handle: handle };
