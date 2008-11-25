7dd504ba9b510ba49594ae9dae3a83a46022c0c2
Will McCutchen <mccutchen@gmail.com>
2008-11-25 14:50:22 -0600

    Make the register view redirect the user back to the page from which they came, if possible

--- views.py
+++ views.py
@@ -20,7 +20,7 @@ def chat(request, slug):
     chat = get_object_or_404(Chat, slug=slug)
     posts = chat.posts.all()
     filters = {}
-    
+
     # Optionally filter posts by keyword (simplistic search)
     # From: http://toastdriven.com/fresh/quick-dirty-search-django/
     q = request.GET.get('q')
@@ -32,13 +32,13 @@ def chat(request, slug):
         # Use operator's or_ to string together all of your Q objects.
         posts = posts.filter(reduce(operator.or_, q_objects))
         filters['search'] = q
-    
+
     # Optionally filter posts by user name
     userfilter = request.GET.get('user')
     if userfilter:
         posts = posts.filter(user__username=userfilter)
         filters['user'] = userfilter
-    
+
     context = {
         'chat': chat,
         'posts': posts,
@@ -53,17 +53,17 @@ def post(request, slug):
         content = request.POST.get('content', '')
         post = Post(user=request.user, parent=chat, content=content)
         post.save()
-        
+
         # Update the user's last ping value
         user_ping(request.user)
-    
+
         if request.is_ajax():
             response = json.dumps({
                 'timestamp': post.timestamp(),
                 'content': force_escape(post.content),
             })
             return HttpResponse(response, mimetype='application/json')
-    
+
     return HttpResponseRedirect(chat_url(slug))
 
 @login_required
@@ -91,19 +91,19 @@ def ping(request, slug):
     if request.POST:
         # Log the ping time for this user
         user_ping(request.user)
-        
+
         # Increasing the timestamp by one second eliminates duplicates
         # on the client side.  This probably merits further thought.
         latest_timestamp = float(request.POST.get('latest', time.time())) + 1
         latest = datetime.datetime.fromtimestamp(latest_timestamp)
-        
+
         # Get a list of posts that were made after the latest time
         # given by the client
         posts_needed = chat.posts.select_related('user').filter(created__gt=latest)
-        
+
         # The response to be serialized as JSON
         response = dict(posts=[], active_users=[], inactive_users=[])
-        
+
         # Fill in the response with posts and users
         for post in posts_needed:
             response['posts'].append({
@@ -111,45 +111,58 @@ def ping(request, slug):
                 'content': post.content,
                 'timestamp': post.timestamp(),
             })
-        
+
         # Add active and inactive users
         for status, users in chat.users().items():
             for user in users:
                 response['%s_users' % status].append(user.username)
-        
+
         # Make sure the current user is included, even it has not
         # contributed
         if request.user.username not in response['active_users']:
             response['active_users'].append(request.user.username)
-        
+
         # Serialize the response as JSON
         return HttpResponse(json.dumps(response), mimetype='application/json')
 
     return HttpResponseServerError()
 
-def user_ping(user):
-    """Updates the given user's last_ping to the current time."""
-    user.last_login = datetime.datetime.now()
-    user.save()
-
-def chat_url(slug):
-    """Utility function that uses the reverse() function to generate the
-    correct URL for the Chat object with the given slug."""
-    return reverse('chat', args=[slug])
-
-
 def register(request):
+    """Register view, to go along with the login and logout views
+    provided by Django's auth framework.  Registers a user and
+    automatically logs them in."""
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            # Save the new user in the database
             new_user = form.save()
+            # Log the user in
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             assert user is not None
             login(request, user)
-            return HttpResponseRedirect("/")
+            return HttpResponseRedirect(request.POST.get('next', '/'))
     else:
         form = UserCreationForm()
+        # Should we redirect the user to a particular page after
+        # registration?
+        next = request.GET.get('next')
+        context = { 'form': form, 'next': next }
+        return direct_to_template(request, "registration/register.html", context)
 
-    return direct_to_template(request, "registration/register.html", {'form':form})
+
+# ====================================================================
+# Utility functions
+# ====================================================================
+def user_ping(user):
+    """Updates the given user's last_login to the current time.  Used
+    to determine which users are still actively participating in a
+    chat."""
+    user.last_login = datetime.datetime.now()
+    user.save()
+
+def chat_url(slug):
+    """Uses the reverse() function to generate the correct URL for the
+    Chat object with the given slug."""
+    return reverse('chat', args=[slug])
