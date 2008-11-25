880332616a7cc7bf0fd92498ba3496accc777715
Will McCutchen <mccutchen@gmail.com>
2008-11-25 14:20:10 -0600

    Add some documentation to urls.py

--- urls.py
+++ urls.py
@@ -7,6 +7,9 @@ from django.contrib import admin
 from models import Chat
 from views import chat, post, create, register, ping
 
+# These options dicts set up the front page (index) and public chat
+# list views, which use Django's generic views and therefore do not
+# have their own dedicated view functions in views.py.
 index_options = {
     'template': 'index.html',
     'extra_context': {
@@ -14,28 +17,39 @@ index_options = {
         'public_chats': lambda: Chat.objects.public()[:5],
     },
 }
-
 chat_list_options = {
     'queryset': Chat.objects.public().order_by('name'),
     'template_object_name': 'chat',
     'template_name': 'chat/chat_list.html',
 }
 
+# The URL patterns for Watercooler
 urlpatterns = patterns('',
+    # Essentially static pages
     url(r'^$', direct_to_template, index_options, name='index'),
     url(r'^about/$', direct_to_template, {'template': 'about.html'}, name='about'),
     url(r'^contact/$', direct_to_template, {'template': 'contact.html'}, name='contact'),
-    
+
+    # The list of public chats
     url(r'^chat/$', object_list, chat_list_options, name='chat_index'),
+    # Create a new chat
     url(r'^chat/create/$', create, name='chat_create'),
+
+    # Views for individual chats, identified by their slug
     url(r'^chat/(?P<slug>[-\w]+)/$', chat, name='chat'),
     url(r'^chat/(?P<slug>[-\w]+)/post/', post, name='chat_post'),
     url(r'^chat/(?P<slug>[-\w]+)/ping/', ping, name='chat_ping'),
-    
+
+    # Authentication views (login and logout are provided by the
+    # Django auth framework, register is provided in views.py)
     (r'^accounts/login/$',  login),
     (r'^accounts/logout/$', logout),
     (r'^accounts/register/$', register),
+
+    # The Django admin site
     (r'^admin/(.*)', admin.site.root),
 )
 
+# Automatically discover and install any admin sites (in this case,
+# just the one admin.py)
 admin.autodiscover()
