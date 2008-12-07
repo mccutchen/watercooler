feb38d9c0832fe95031958bc1f0cb9f861c1dc39
Will McCutchen <mccutchen@gmail.com>
2008-12-07 14:25:18 -0600

    Also check for errors in the username.

--- media/js/reg.js
+++ media/js/reg.js
@@ -3,17 +3,32 @@ $(function() {
     // Before the registration form is submitted, ensure that the
     // passwords match.
     $('#register').submit(function(event) {
-        var a = $('#id_password1').val();
-        var b = $('#id_password2').val();
+        // Gather up the data we're validating
+        var thisForm = $(this);
+        var userName = $('#id_username').val();
+        var p1 = $('#id_password1').val();
+        var p2 = $('#id_password2').val();
+
+        // Collect any validation errors
+        var errors = [];
+
+        // Make sure the username is valid
+        if (!/^\w+$/.test(userName))
+            errors.push('Username must contain only letters, numbers, or underscores.');
+
+        // Make sure the passwords match
+        if (p1 != p2)
+            errors.push('Passwords do not match.');
         
-        // If the passwords don't match, report the error and
-        // cancel the form submission.
-        if (a != b) {
-            var error = '<p class="error">Passwords do not match.</p>';
-            $(this).before(error);
+        // If we got a validation error, display an error message to
+        // the user and cancel form submission.
+        if (errors.length > 0) {
+            errors.each(function(e) {
+                thisForm.before('<p class="error">' + e + '</p>');
+            });
             return false;
         }
-        
+
         // Otherwise, allow the form submission to continue.
         return true;
     });
