a91f51f3d516526cedd65ffa98be7777816b638f
Will McCutchen <mccutchen@gmail.com>
2008-12-07 19:12:40 -0600

    Removed fabfile from version control

--- fabfile.py
+++
@@ -1,9 +0,0 @@
-set(
-    fab_hosts = ['overloaded.org'],
-)
-
-def deploy():
-    """Update and restart Watercooler."""
-    local('hg push')
-    run('cd ~/projects/watercooler; hg pull -u')
-    run('~/etc/rc.d/watercooler.sh restart')
