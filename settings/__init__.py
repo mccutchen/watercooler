c34f92953410d7ff4e6c09e1ea24aa143597d85f
Will McCutchen <mccutchen@gmail.com>
2008-12-07 20:14:58 -0600

    Improve documentation of settings

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,17 +1,26 @@
 import os
+
+# --------------------------------------------------------------------
+# Watercooler-specific settings
+# --------------------------------------------------------------------
+
+# Find the absolute path to this project's source code (which will be
+# used in the settings below).
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
 
 # How many seconds of inactivity before a user is considered inactive?
 ACTIVE_USER_TIMEOUT = 20
 
+
+# --------------------------------------------------------------------
+# Django settings for Watercooler
+# --------------------------------------------------------------------
 ADMINS = (
     ('Will McCutchen', 'mccutchen@gmail.com'),
 )
 MANAGERS = ADMINS
 
 ROOT_URLCONF = 'watercooler.urls'
-
-AUTH_PROFILE_MODULE = 'watercooler.UserProfile'
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -32,7 +41,9 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
+    # Adds the MEDIA_URL setting to the context
     'django.core.context_processors.media',
+    # Adds the current user to the context
     'django.core.context_processors.auth',
 )
 
@@ -52,13 +63,19 @@ SITE_ID = 1
 USE_I18N = True
 
 
-# Import specific settings based on hostname
+# --------------------------------------------------------------------
+# Deployment-specific settings
+#
+# First, try importing settings for the current host.  Then try
+# importing "local" settings (which wouldn't be kept under version
+# control).  Ignore any errors.
+# --------------------------------------------------------------------
 try:
     import socket, re
     hostname = re.sub(r'[-\.]', '_', socket.gethostname())
     exec "from %s import *" % hostname
-except ImportError, e:
-    raise e
+except ImportError:
+    pass
 
 # Import local settings (which aren't kept in version control)
 try:
